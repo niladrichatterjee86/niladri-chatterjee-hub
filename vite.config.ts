b5bb1f2f79c828415f6acd07bd78a275a0751a99
niladrichatterjee86 <niladri.chatterjee86@gmail.com>
2026-05-24 20:17:11 +0800

    close out configuration brackets cleanly

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
@@ -20,3 +20,4 @@ export default defineConfig(() => {
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
+});
